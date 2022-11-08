641201514b12db73c83c15c2e7e639e95d449cce
Hector Zapata <hzapata9@gmail.com>
2022-11-07 21:31:48 -0300

    Punto 5 - Modificacion archivo RestData

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -7,12 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.devopsusach2020.model.Pais;
 import com.devopsusach2020.model.Mundial;
 import com.google.gson.Gson;
 
@@ -23,13 +21,21 @@ public class RestData {
 	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
 	
-	@GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message){
+	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Mundial getTotalMundial(){
 		
-		LOGGER.log(Level.INFO, "Proceso exitoso de prueba");
+		LOGGER.log(Level.INFO, "Consulta mundial");
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> call = restTemplate.getForEntity("https://api.covid19api.com/world/total", String.class);
 		
-		Pais response = new Pais();
-		response.setMensaje("Mensaje Recibido: " + message);
+		Mundial response = new Mundial();
+		Gson gson = new Gson();
+
+		Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
+		response.setTotalConfirmed(estado.getTotalConfirmed());
+		response.setTotalDeaths(estado.getTotalDeaths());
+		response.setTotalRecovered(estado.getTotalRecovered());
 		return response;
 	}
 }
